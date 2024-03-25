4ec1334eac23d466875b947c0e82095a32c6e28c
abubakirmajidov <abubakirmajidov@gmail.com>
2024-03-24 16:56:38 -1200

    5.2dars-.js

---
+++ js/app.js
@@ -0,0 +1,42 @@
+// const num1 = +prompt("Birinchi");
+// const amal = prompt("amal");
+// const num2 = +prompt("Ikkinchi");
+
+// const title = document.getElementById("title");
+
+// switch (amal) {
+//   case "+":
+//     title.textContent = `${num1} + ${num2} = ${num1 + num2} `;
+//     break;
+//   case "-":
+//     title.textContent = `${num1} - ${num2} = ${num1 - num2} `;
+//     break;
+//   case "*":
+//     title.textContent = `${num1} * ${num2} = ${num1 * num2} `;
+//     break;
+//   case "/":
+//     title.textContent = `${num1} / ${num2} = ${num1 / num2} `;
+//     break;
+// }
+
+// const li = document.getElementsByTagName("li");
+
+// for (let i = 0; i < li.length; i++) {
+//   li[i].style.color = "orangered";
+// }
+
+// console.log(li);
+
+//////////////////////////////////////////////////////////////
+
+// const li = document.querySelectorAll("li");
+
+// li.forEach((item) => {
+//   item.textContent = Math.trunc(Math.random() * 10);
+//   item.textContent = randomNumber;
+//   if (randomNumber % 2 == 0) {
+//     item.style.borderColor = "red";
+//   } else {
+//     item.style.borderColor = "blak";
+//   }
+// });
---
+++ js/app2.js
@@ -0,0 +1,6 @@
+let a = 13;
+function callme() {
+  console.log("You call me !");
+}
+
+export { a, callme };
